b75c2a37852059d33a88365a93a5926891f5c476
Nubes-Vespertina <makikumo@126.com>
2025-10-17 21:32:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-Python 3.14.0 (v3.14.0:ebf955df7a8, Oct  7 2025, 08:20:14) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
->>> # app.py - 最终部署版本
+ # app.py - 最终部署版本
 ... import streamlit as st
 ... import random
 ... import math
